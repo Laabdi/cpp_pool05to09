21f97e049cab00a8679cd23195f1ca5ccb4a7f5b
moaregra <moaregra@c4r3p2.1337.ma>
2025-07-04 08:21:29 +0100

    ex01

--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -3,7 +3,32 @@
 
 class Form
 {
-    private :
-    const std::string name;
-    bool sign;
+  private:
+	const std::string name;
+	bool signature;
+	const int gradeToSign;
+	const int gradeToExecute;
+
+  public:
+	Form();
+	Form::Form(const std::string &Name, const int gradetosign,const int gradetoexecute) : name(Name), signature(false),gradeToSign(gradetosign), gradeToExecute(gradetoexecute)
+	{
+		// You can add validation or other logic here if needed
+	}
+	~Form();
+
+	class GradeTooHighException : public std::exception
+	{
+		const char *what() throw()
+		{
+			return ("Grade too High");
+		}
+	};
+	class GradeTooLowException : public std::exception
+	{
+		const char *what() throw()
+		{
+			return ("Grade too Low");
+		}
+	};
 };
