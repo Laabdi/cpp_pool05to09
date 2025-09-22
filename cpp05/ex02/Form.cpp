441969a88060b1b3dcb1bfecf6a54fb8cba85fe3
Laabdi <chinwinouad04@gmail.com>
2025-09-22 20:57:15 +0100

    going on

---
+++ cpp05/ex02/Form.cpp
@@ -0,0 +1,54 @@
+#include "Form.hpp"
+
+Form::Form(const std::string &Name, const int gradetosign,
+	const int gradetoexecute) : name(Name), signature(false),
+	gradeToSign(gradetosign), gradeToExecute(gradetoexecute)
+{
+	if (gradeToSign < 1 || gradeToExecute < 1)
+		throw GradeTooHighException();
+	if (gradeToSign > 150 || gradeToExecute > 150)
+		throw GradeTooLowException();
+}
+std::string Form::getName() const
+{
+	return (name);
+}
+
+bool Form::isSigned() const
+{
+	return (signature);
+}
+
+int Form::getGradeToSign() const
+{
+	return (gradeToSign);
+}
+
+int Form::getGradeToExecute() const
+{
+	return (gradeToExecute);
+}
+void Form::beSigned(Bureaucrat &bureaucrat)
+{
+	if (bureaucrat.get_grade() <= gradeToSign)
+		this->signature = true;
+	else
+		throw GradeTooLowException();
+}
+
+const char *Form::GradeTooHighException::what() const throw()
+{
+	return ("Grade too High");
+}
+const char *Form::GradeTooLowException::what() const throw()
+{
+	return ("Grade too Low");
+}
+Form::~Form()
+{
+}
+std::ostream &operator<<(std::ostream &out, const Form &form)
+{
+	out << "Form \"" << form.getName() << "\" | Signed: " << (form.isSigned() ? "yes" : "no") << " | Grade to sign: " << form.getGradeToSign() << " | Grade to execute: " << form.getGradeToExecute();
+	return (out);
+}
