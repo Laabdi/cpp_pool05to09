ca61b154e62cc4196ddbc625e88a37cd25fa9fa6
Laabdi <chinwinouad04@gmail.com>
2025-12-10 16:05:37 +0100

    push

--- cpp06/ex02/helpers.cpp
+++ cpp06/ex02/helpers.cpp
@@ -8,7 +8,8 @@
 Base *generate(void)
 {
     static bool seeded = false;
-    if (!seeded) {
+    if (!seeded) 
+    {
         srand(time(NULL));
         seeded = true;
     }
@@ -17,17 +18,14 @@ Base *generate(void)
     
     if (random == 0)
     {
-        std::cout << "A" << std::endl;
         return new A(); 
     }
     else if (random == 1)
     {
-        std::cout << "B" << std::endl;
         return new B(); 
     }
     else
     {
-        std::cout << "C" << std::endl;
         return new C();
     }
 }
@@ -43,10 +41,34 @@ void identify(Base* p)
 }
 void identify(Base& p)
 {
+    try 
+    {
+        dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return;
+    }
+    catch(std::bad_cast&){}
+    try 
+    {
+        dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return;
 
+    }    
+        catch(std::bad_cast&){}
+    try 
+    {
+        dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return;
+
+    }
+        catch(std::bad_cast&){}
 }
 int main()
 {
     Base *p = generate();
+    Base *t =generate();
+    identify(t);
     identify(p);
 }
